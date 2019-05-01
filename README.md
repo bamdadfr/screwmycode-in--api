cb86478ab16caeed1f7a1339d37f8f385f0084bd
Bamdad Sabbagh <dev@bamdadsabbagh.com>
2019-05-01 16:30:55 +0200

    a

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # learnExpress
+
+* https://www.youtube.com/watch?v=0oXYLzuucwE&list=PL55RiY5tL51q4D-B63KBnygU6opNPFk_q
+
+## Documentation
+### Express
+* https://expressjs.com/en/starter/installing.html
+* https://expressjs.com/en/starter/hello-world.html
+* https://expressjs.com/en/guide/routing.html
+
+### GraphQL
+* https://graphql.org/graphql-js/running-an-express-graphql-server/
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => 'Hello world!',
+}
 
 const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}))
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
