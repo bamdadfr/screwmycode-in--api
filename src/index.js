13489c1fadde8d4b36f1a26382c33bd3d681d8f5
Bamdad Sabbagh <dev@bamdadsabbagh.com>
2019-05-10 15:14:56 +0200

    not sending a request if url is not 200

--- src/index.js
+++ src/index.js
@@ -2,50 +2,101 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const { exec } = require('child_process')
+const https = require('https')
 
 // INIT
 
 const app = express()
 const port = 5000
-const youtubeUrl = 'https://www.youtube.com/watch?v='
 
-const regEx = /^([0-9A-Za-z_-]{11})$/
+/*
 
-// API Service
+  GLOBAL FUNCTIONS
+
+ */
+
+
+const isUrl200 = url => new Promise((resolve, reject) => {
+  https.get(url, (r) => {
+    if (r.statusCode === 200) {
+      resolve(true)
+    } else {
+      resolve(false)
+    }
+  })
+})
+
+const execShellCommand = (baseUrl, id) => new Promise((resolve, reject) => {
+  exec(`youtube-dl -e -f 140 -g ${baseUrl}${id}`, (err, stdout, stderr) => {
+    const [title, url] = stdout.split('\n')
+
+    isUrl200(url)
+      .then((is200) => {
+        if (is200) {
+          resolve({
+            success: true,
+            title,
+            url,
+            err: stderr,
+          })
+        } else {
+          reject({
+            success: false,
+            err: '403',
+          })
+        }
+      })
+  })
+})
+
+const execShellCommandUntilSuccess = async (baseUrl, id) => {
+  try {
+    return await execShellCommand(baseUrl, id)
+  } catch (err) {
+    console.log('403 catched, retrying...', err)
+    return await execShellCommand(baseUrl, id)
+  }
+}
+
+/*
+
+  GLOBAL
+  ENDPOINT
+
+ */
+
 
 app.use(helmet())
 
 app.get('/', (req, res) => {
-  res.send('Hello')
+  res.send({
+    success: true,
+    message: 'Hello',
+  })
 })
+
+/*
+
+  YOUTUBE
+  ENDPOINT
+
+ */
 
 // Use CORS() for local dev
 // remove it for production when behind a Apache Reverse Proxy
 
-// app.get('/youtube/:id', cors(), (req, res) => {
-app.get('/youtube/:id', (req, res) => {
+app.get('/youtube/:id', cors(), (req, res) => {
+// app.get('/youtube/:id', (req, res) => {
+
   const { id } = req.params
-  console.log('id', id)
+  console.log(new Date(), 'YouTube:', id)
+
+  const baseUrl = 'https://www.youtube.com/watch?v='
+  const regEx = /^([0-9A-Za-z_-]{11})$/
 
   if (regEx.test(id)) {
-    exec(`youtube-dl -e -f 140 -g ${youtubeUrl}${id}`, (err, stdout, stderr) => {
-      if (err) { return }
-
-      const response = stdout.split('\n')
-      const [title, url] = response
-
-      res.send({
-        success: true,
-        title,
-        url,
-        // err: stderr,
-      })
-    })
-  } else {
-    res.send({
-      success: false,
-      err: 'Wrong YouTube ID',
-    })
+    execShellCommandUntilSuccess(baseUrl, id)
+      .then(r => res.send(r))
   }
 })
 
