c55b03acad19488d98f680f8ce0c34db3f406b8e
Bamdad Sabbagh <git@bamdadsabbagh.com>
2020-07-10 18:56:16 +0200

    fix: writing query tests

---
+++ src/youtube.queries.test.ts
@@ -0,0 +1,48 @@
+import request from 'supertest'
+import mongoose from 'mongoose'
+import app from './app'
+import { getMongoURL } from './mongo.utils'
+import { YoutubeController } from './youtube.controller'
+import { IYoutubeControllerResponse } from './youtube.types'
+import { YoutubeQueryRead } from './youtube.queries'
+
+describe ('testinging youtube queries', () => {
+
+    beforeAll (() => {
+
+        mongoose.connect (getMongoURL (), {
+            'useNewUrlParser': true,
+            'useUnifiedTopology': true,
+        }) 
+    
+    })
+
+    afterAll (() => {
+
+        mongoose.disconnect ()
+    
+    })
+
+    // https://www.youtube.com/watch?v=UY6dvVeuzk4
+    it ('should succeed creating youtube entry', async done => {
+
+        const response: IYoutubeControllerResponse = await YoutubeController ('UY6dvVeuzk4')
+
+        expect (response.success).toBeTruthy ()
+
+        done ()
+
+    })
+
+    it ('should succeed reading youtube entry', async done => {
+
+        const response = await YoutubeQueryRead ('UY6dvVeuzk4')
+
+        expect (response.success).toBeTruthy ()
+
+        done ()
+    
+    })
+
+})
+
