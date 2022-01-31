4a0e8c108e49c0d5f4f677de4f16f5438c00df69
Bamdad Sabbagh <hi@bamdad.fr>
2022-01-31 08:41:05 +0100

    feat(Soundcloud): Add piped media to cache stream URL, reducing Soundcloud requests and better hit detection + Various improvements and code cleaning

--- src/domains/soundcloud/utils/get-soundcloud-stream.ts
+++ src/domains/soundcloud/utils/get-soundcloud-stream.ts
@@ -1,6 +1,6 @@
 import { Soundcloud } from '../schemas/soundcloud.schema.js';
 import SoundcloudScraper from 'soundcloud-scraper';
 
-export function getSoundcloudStream(soundcloud: Soundcloud) {
-  return SoundcloudScraper.Util.fetchSongStreamURL(soundcloud.audio, undefined);
+export function getSoundcloudStream(url: Soundcloud['audio']) {
+  return SoundcloudScraper.Util.fetchSongStreamURL(url, undefined);
 }
--- src/domains/youtube/utils/generate-piped-youtube-image.ts
+++
@@ -1,6 +0,0 @@
-import { getDomain } from '../../../utils/get-domain.js';
-import { Youtube } from '../schemas/youtube.schema.js';
-
-export function generatePipedYoutubeImage(entity: Youtube): string {
-  return `${getDomain()}/youtube/${entity.id}/image`;
-}
