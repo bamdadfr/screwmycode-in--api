48e645344d86e03d468535d4c42aed17858b3a62
Bamdad Sabbagh <hi@bamdad.fr>
2022-02-01 10:22:15 +0100

    perf(Root): Move response builder at controller level + Add utility function to convert documents to response objects + Fix DTO types

--- src/domains/root/root.service.ts
+++ src/domains/root/root.service.ts
@@ -5,15 +5,6 @@ import {
   Soundcloud,
   SoundcloudDocument,
 } from '../soundcloud/schemas/soundcloud.schema.js';
-import {
-  RootLatestDto,
-  RootSoundcloudLatest,
-  RootSoundcloudTop,
-  RootTopDto,
-  RootYoutubeLatest,
-  RootYoutubeTop,
-} from './dto/root.dto.js';
-import { getDomain } from '../../utils/get-domain.js';
 
 export class RootService {
   constructor(
@@ -23,71 +14,51 @@ export class RootService {
     private readonly soundcloudModel: mongoose.Model<SoundcloudDocument>,
   ) {}
 
-  async readAllByHits(limit = 10): Promise<RootTopDto> {
+  async readAllByHits(limit = 10): Promise<{
+    youtubes: YoutubeDocument[];
+    soundclouds: SoundcloudDocument[];
+  }> {
+    const fields = '-_id id hits title image';
+
     const youtubes = await this.youtubeModel
       .find()
-      .select('-_id id hits title image')
+      .select(fields)
       .sort({ hits: -1 })
       .limit(limit);
 
     const soundclouds = await this.soundcloudModel
       .find()
-      .select('-_id id hits title image')
+      .select(fields)
       .sort({ hits: -1 })
       .limit(limit);
 
-    const typedYoutubes: RootYoutubeTop[] = youtubes.map((youtube) => {
-      return {
-        ...youtube.toObject(),
-        image: `${getDomain()}/youtube/${youtube.id}/image`,
-        type: 'youtube',
-      };
-    });
-
-    const typedSoundclouds: RootSoundcloudTop[] = soundclouds.map(
-      (soundcloud) => {
-        return {
-          ...soundcloud.toObject(),
-          image: `${getDomain()}/soundcloud/${soundcloud.id}/image`,
-          type: 'soundcloud',
-        };
-      },
-    );
-
-    return [...typedYoutubes, ...typedSoundclouds];
+    return {
+      youtubes,
+      soundclouds,
+    };
   }
 
-  async readAllByDate(limit = 10): Promise<RootLatestDto> {
+  async readAllByDate(limit = 10): Promise<{
+    youtubes: YoutubeDocument[];
+    soundclouds: SoundcloudDocument[];
+  }> {
+    const fields = '-_id id hits title image updatedAt';
+
     const youtubes = await this.youtubeModel
       .find()
-      .select('-_id id hits title image updatedAt')
+      .select(fields)
       .sort({ updatedAt: -1 })
       .limit(limit);
 
     const soundclouds = await this.soundcloudModel
       .find()
-      .select('-_id id hits title image updatedAt')
+      .select(fields)
       .sort({ updatedAt: -1 })
       .limit(limit);
 
-    const typedYoutubes: RootYoutubeLatest[] = youtubes.map((youtube) => {
-      return {
-        ...youtube.toObject(),
-        image: `${getDomain()}/youtube/${youtube.id}/image`,
-        type: 'youtube',
-      };
-    });
-
-    const typedSoundclouds: RootSoundcloudLatest[] = soundclouds.map(
-      (soundcloud) => {
-        return {
-          ...soundcloud.toObject(),
-          image: `${getDomain()}/soundcloud/${soundcloud.id}/image`,
-          type: 'soundcloud',
-        };
-      },
-    );
-
-    return [...typedYoutubes, ...typedSoundclouds];
+    return {
+      youtubes,
+      soundclouds,
+    };
   }
 }
