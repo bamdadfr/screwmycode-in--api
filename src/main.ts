6d1b1060ff729ecbe7b41fab9f0ac0563d349122
Bamdad Sabbagh <hi@bamdad.fr>
2022-08-26 06:01:05 +0200

    perf: Remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module.js';
 import { isEnvProduction } from './utils/is-env-production.js';
 
@@ -24,17 +23,6 @@ async function bootstrap() {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   app.use(helmet());
-
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('title example')
-    .setDescription('description example')
-    .setVersion('1.0')
-    .addTag('tag example')
-    .build();
-
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-
-  SwaggerModule.setup('docs', app, swaggerDocument);
 
   await app.listen(3000);
 }
