04e94470254ec092175100d23bc9e06739333921
Bamdad Sabbagh <hi@bamdad.fr>
2024-07-11 22:18:03 +0200

    fix: add youtube_dl purge command to restart helper

--- restart.sh
+++ restart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source /home/aent4329/virtualenv/screwmycodein/3.11/bin/activate && cd /home/aent4329/screwmycodein
+youtube-dl --rm-cache-dir
+rm -r /home/aent4329/virtualenv/screwmycodein/3.11/lib/python3.11/site-packages/youtube_dl*
 pip install -r requirements.txt
 touch tmp/restart.txt
 deactivate
