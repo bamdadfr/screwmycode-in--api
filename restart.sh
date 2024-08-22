5386e3657fcf21fda64c44ff21914032567707af
Bamdad Sabbagh <hi@bamdad.fr>
2024-08-22 23:37:32 +0200

    fix: replace restart script

--- restart.sh
+++ restart.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 source /home/aent4329/virtualenv/screwmycodein/3.11/bin/activate && cd /home/aent4329/screwmycodein
-youtube-dl --rm-cache-dir
-rm -r /home/aent4329/virtualenv/screwmycodein/3.11/lib/python3.11/site-packages/youtube_dl*
 pip install -r requirements.txt
 touch tmp/restart.txt
 deactivate
