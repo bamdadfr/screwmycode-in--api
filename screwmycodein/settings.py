aa5c954020eb594225da134ca97b6fe797df015a
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-07 16:03:19 +0200

    fix: add 200 status code middleware

--- screwmycodein/settings.py
+++ screwmycodein/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "screwmycodein.middleware.success_middleware",
     "screwmycodein.middleware.audio_middleware",
     "screwmycodein.middleware.image_middleware",
 ]
