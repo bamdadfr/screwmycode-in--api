d700311575078093deabb2b948ee646f4ac7504c
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-08 03:02:55 +0200

    fix: update webhook

--- webhook-deploy.sh
+++ webhook-deploy.sh
@@ -6,7 +6,11 @@ app_path="$2"
 # shellcheck source=./deploy.sh
 source "${venv_path}" && cd "${app_path}" || exit 1
 git pull
+
+python -m pip install --upgrade pip
 pip install -r requirements.txt
+
 python manage.py migrate
 touch tmp/restart.txt
+
 deactivate
