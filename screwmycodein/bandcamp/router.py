734cd7f665e96576aa057cf7675054db2120a384
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-11 01:54:34 +0200

    fix: add back image endpoints

--- screwmycodein/bandcamp/router.py
+++ screwmycodein/bandcamp/router.py
@@ -1,9 +1,10 @@
 from django.core.handlers.wsgi import WSGIRequest
 from ninja import Router
 
-from ..audio.dto import AudioDtoFullResponse
+from ..audio.dto import AudioDtoFullResponse, AudioDtoStreamResponse
 from ..audio.services import AudioService
 from ..audio.utils import AudioUtil
+from ..utils.proxy import Proxy
 from ..utils.youtube_dl_utils import YoutubeDlUtil
 
 router = Router()
@@ -20,3 +21,17 @@ def root(request: WSGIRequest):
 def bandcamp_index(request: WSGIRequest, artist: str, name: str):
     row = AudioService.find_or_create_bandcamp(artist, name)
     return AudioUtil.serialize(row)
+
+
+@router.api_operation(
+    ["GET", "HEAD"],
+    "{artist}/{name}/image",
+    response=AudioDtoStreamResponse,
+)
+def soundcloud_image(
+    request: WSGIRequest,
+    artist: str,
+    name: str,
+):
+    row = AudioService.find_or_create_soundcloud(artist, name)
+    return Proxy.stream_remote(row.image)
--- screwmycodein/youtube/router.py
+++ screwmycodein/youtube/router.py
@@ -2,9 +2,10 @@ from django.core.handlers.wsgi import WSGIRequest
 from ninja import Router
 
 from .utils import YoutubeUtil
-from ..audio.dto import AudioDtoFullResponse
+from ..audio.dto import AudioDtoFullResponse, AudioDtoStreamResponse
 from ..audio.services import AudioService
 from ..audio.utils import AudioUtil
+from ..utils.proxy import Proxy
 from ..utils.youtube_dl_utils import YoutubeDlUtil
 
 router = Router()
@@ -22,3 +23,17 @@ def root(request: WSGIRequest):
 def youtube_index(request: WSGIRequest, slug: str):
     row = AudioService.find_or_create_youtube(slug)
     return AudioUtil.serialize(row)
+
+
+@router.api_operation(
+    ["GET", "HEAD"],
+    "{youtube_id}/image",
+    response=AudioDtoStreamResponse,
+)
+@YoutubeUtil.validate_id
+def youtube_image(
+    request: WSGIRequest,
+    slug: str,
+):
+    row = AudioService.find_or_create_youtube(slug)
+    return Proxy.stream_remote(row.image)
