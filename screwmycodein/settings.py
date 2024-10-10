643d353b24dc9385564e296efaeeced1ee95e475
Bamdad Sabbagh <hi@bamdad.fr>
2024-10-10 19:57:01 +0200

    fix: allow 4 bytes utf8 chars

--- screwmycodein/settings.py
+++ screwmycodein/settings.py
@@ -88,6 +88,7 @@ DATABASES = {
         "PORT": config.mysql_port,
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
         },
     }
 }
