350d3f052ad50ec17e333d2fe1896b75357d188e
Bamdad Sabbagh <hi@bamdad.fr>
2025-07-29 22:07:42 +0200

    fix: disable v1 routes

--- screwmycodein/screwmycodein/api.py
+++ screwmycodein/screwmycodein/api.py
@@ -4,10 +4,14 @@ api = NinjaAPI()
 
 api.add_router("/", "screwmycodein.home.api.router")
 api.add_router("/webhook", "screwmycodein.webhook.api.router")
-api.add_router("/top", "screwmycodein.top.api.router")
-api.add_router("/last", "screwmycodein.last.api.router")
-api.add_router("/youtube", "screwmycodein.youtube.api.router")
-api.add_router("/soundcloud", "screwmycodein.soundcloud.api.router")
-api.add_router("/bandcamp", "screwmycodein.bandcamp.api.router")
+
+# v1 routes
+# api.add_router("/top", "screwmycodein.top.api.router")
+# api.add_router("/last", "screwmycodein.last.api.router")
+# api.add_router("/youtube", "screwmycodein.youtube.api.router")
+# api.add_router("/soundcloud", "screwmycodein.soundcloud.api.router")
+# api.add_router("/bandcamp", "screwmycodein.bandcamp.api.router")
+
+# v2 routes
 api.add_router("/v2/media", "screwmycodein.v2.media.api.router")
 api.add_router("/v2/list", "screwmycodein.v2.list.api.router")
