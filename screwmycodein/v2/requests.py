6c51941c1144260ac9cf2522ced83062089024b7
Bamdad Sabbagh <hi@bamdad.fr>
2025-07-30 22:57:26 +0200

    fix: try to catch first audio request

--- screwmycodein/v2/requests.py
+++ screwmycodein/v2/requests.py
@@ -2,4 +2,8 @@ from django.core.handlers.wsgi import WSGIRequest
 
 
 def is_new_request(request: WSGIRequest) -> bool:
-    return request.headers.get("Range") == "bytes=0-"
+    # works on local env
+    # return request.headers.get("Range") == "bytes=0-"
+
+    # works on prod?
+    return request.headers.get("Range") is None
